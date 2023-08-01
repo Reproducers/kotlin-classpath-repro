98c7bc8fdc5b84122beda7e6d7dfc8ef60cd8df9
Javier Segovia Córdoba <javier@segoviacordoba.com>
2023-08-01 18:29:00 +0200

    Add repro

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+buildscript { //
+    dependencies { //
+        classpath(libs.kotlin.gradlePlugin)
+    }
+}
+
+plugins { //
+    alias(libs.plugins.javiersc.hubdle)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,47 @@
+pluginManagement {
+    val hubdleVersion: String =
+        file("$rootDir/gradle/libs.versions.toml")
+            .readLines()
+            .first { it.contains("hubdle") }
+            .split("\"")[1]
+
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        google()
+        maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap")
+        maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
+        maven("https://oss.sonatype.org/content/repositories/snapshots")
+    }
+
+    plugins { //
+        id("com.javiersc.hubdle") version hubdleVersion
+    }
+}
+
+plugins { //
+    id("com.javiersc.hubdle")
+}
+
+dependencyResolutionManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        google()
+        maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap")
+        maven(url = "https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
+        sonatypeSnapshot()
+    }
+}
+
+val kotlinVersion: String =
+    file("$rootDir/gradle/libs.versions.toml")
+        .readLines()
+        .first { it.contains("kotlin") }
+        .split("\"")[1]
+
+hubdleSettings {
+    catalog { //
+        replaceStrictVersion("kotlin" to kotlinVersion)
+    }
+}
